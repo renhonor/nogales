4cf1ed43004cd9d985794de63eb86c7f2c21e7df
renhonor <66085236+renhonor@users.noreply.github.com>
2026-07-21 11:05:50 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { createClient, User } from '@supabase/supabase-js';
-import jsPDF from 'jspdf';
+import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 import { 
   Plus, 
